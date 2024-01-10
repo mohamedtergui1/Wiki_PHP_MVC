b37a8899fd11979ea8fe81aa35d9b96b9f31ee3f
mohamed <simotergui4@gmail.com>
2024-01-10 15:14:37 +0100

    add validation to insert wiki

--- app/controller/AddwikiController.php
+++ app/controller/AddwikiController.php
@@ -4,7 +4,7 @@
   use App\Model\TagModel;
   use App\Model\WikiModel;
   use App\Model\WikiTagModel;
-
+  use App\Hepler\Helper;
   class AddwikiController extends Controller{
       private $category ;
       private $tag ;
@@ -27,7 +27,7 @@
       function insert(){
         if($_SERVER["REQUEST_METHOD"]=="POST"){
           
-         
+         if($this->validate($_POST) == "1"){
           if($_POST["tagId"]){
              $tags = $_POST["tagId"];
              unset($_POST["tagId"]);
@@ -44,6 +44,21 @@
           } else echo " error insert wiki";
            
 
-        }else echo " no data recived";
+        }else echo $this->validate($_POST);
       }
+    }
+     
+    function validate(array $data){
+         
+          if(Helper::validateName($data["title"])) return "you title npt valide";
+               
+          else if(strlen(trim($data['title']))<4) return "yout title so short";
+
+          else if(Helper::validateString($data["content"])) return "your wiki must be text";
+
+          else if (strlen(trim($data['content']))) return "your content is short enter olud then 30 char"; 
+
+          else return "1";
+          
+    }
   }
--- app/helper/Helper.php
+++ app/helper/Helper.php
@@ -2,5 +2,31 @@
    namespace App\Hepler;
 
    class Helper{
-       
+    public static function validateString($input)
+    {
+        
+        return !is_string($input) && !empty($input);
+    }
+
+    public static function validateInteger($input)
+    {
+        
+        return !filter_var($input, FILTER_VALIDATE_INT) !== false;
+    }
+
+    public static function validateEmail($input)
+    {
+        
+        return !filter_var($input, FILTER_VALIDATE_EMAIL) !== false;
+    }
+
+    public static function validateAlphaNumeric($input)
+    {
+        
+        return !ctype_alnum($input);
+    }
+   public static function validateName($input){
+        return !preg_match('/^[a-zA-Z-\s]+$/', $input);
+   }
+   
    }
