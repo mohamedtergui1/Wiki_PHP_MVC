8d2aa6b0ce6525e1b3d4f1ab0df89f18384f584a
mohamed <simotergui4@gmail.com>
2024-01-14 18:11:53 +0100

    add page profile

--- app/controller/AddwikiController.php
+++ app/controller/AddwikiController.php
@@ -15,7 +15,7 @@
         $this->category = new CategoryModel;
         $this->tag = new TagModel;
         $this->wiki = new WikiModel;
-        $this->wikiTag =  new WikiTagModel; 
+    
       }
       function index(){
           if($this->idFromSession){
@@ -46,10 +46,10 @@
                $id_wiki = $this->wiki->lastId();
                if($id_wiki){
                     foreach($tags as $t ){
-                          $this->wikiTag->insert(["wikiID" => $id_wiki, "tagID" => $t]);
+                          $this->wiki->insertWikiTag(["wikiID" => $id_wiki, "tagID" => $t]);
                     }
                     echo "1";
-               }else echo "not fount last id inserdet";
+               }else echo "not fount last id inserdet"; 
           } else echo " error insert wiki";
            
 
